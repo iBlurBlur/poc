8d8fcfa5b63453d1f9b57640626ca000a4addc16
iBlurBlur <tanakorn.ngam@gmail.com>
2021-10-19 10:12:47 +0700

    init

---
+++ DemoReport/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using AspNetCore.Reporting;
+using DemoReport.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoReport.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment _webHostEnvironment)
+        {
+            _logger = logger;
+            webHostEnvironment = _webHostEnvironment;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Print()
+        {
+            string mimeType = "";
+            int extension = 1;
+            var path = $"{webHostEnvironment.WebRootPath}\\Reports\\Report1.rdlc";
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("Name", "iblurblur");
+            var localReport = new LocalReport(path);
+            var result = localReport.Execute(RenderType.Pdf, extension, parameters, mimeType);
+            return File(result.MainStream, "application/pdf");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
